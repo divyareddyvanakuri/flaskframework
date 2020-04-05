8795ebe18eec2b8c72319309b23abaed0caadc29
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-04-05 15:19:41 +0530

    [Divya]refactor: implemented the functional logic of create cursor for database and created the table in database method

--- flaskproject/app.py
+++ flaskproject/app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 
+
 app = Flask(__name__)
 
 #mysql database connection 
 app.config["MYSQL_HOST"]="localhost"
+app.config["MYSQL_PORT"]=3306
 app.config["MYSQL_USER"]="Divya"
 app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"]="flask"
+app.config["MYSQL_CURSORCLASS"]="DictCursor"
 
 mysql = MySQL(app)
 
@@ -23,6 +26,12 @@ def login():
 def register():
    return "Please register"
 
-
+@app.route('/database')
+def database():
+   cur = mysql.connection.cursor()
+   cur.execute('''CREATE TABLE example (id INTEGER ,name VARCHAR(20))''')
+   # mysql.connection.commit()
+   cur.close()
+   return "Done!"
 if __name__ == '__main__':
    app.run(debug=True)
