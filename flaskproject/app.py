72ef79d0dad953be2f3f5e4bdb626d1f9177a2d9
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-04-07 12:57:49 +0530

    [Divya]refactor: in login and register method password storing in bd by bcrypt way and forgotpassword fucntional logic change by adding db select query

--- flaskproject/app.py
+++ flaskproject/app.py
@@ -1,9 +1,18 @@
 from flask import Flask,render_template,request,session,redirect,url_for
 from flask_mysqldb import MySQL,MySQLdb
 from MySQLdb.connections import OperationalError
+from flask_mail import Mail
+from flask_shorturl import ShortUrl
+from werkzeug.security import generate_password_hash,check_password_hash
+# import bcrypt
 import bcrypt
+import datetime
+import jwt
+
+
 
 app = Flask(__name__)
+mail = Mail(app)
 app.config["SECRET_KEY"]="exobts2020@#$%^^&"
 #mysql database connection 
 app.config["MYSQL_HOST"]="localhost"
@@ -31,7 +40,7 @@ def login():
       user = cur.fetchone()
       print(user)
       cur.close()
-      if password == user["password"]:
+      if check_password_hash(user["password"], password,method="sha256"):
          session["username"] = user["username"]
          session["email"] = user["email"]
          return render_template("home.html")
@@ -51,9 +60,11 @@ def register():
       email=request.form["email"]
       password=request.form["password"]
       confirmpassword=request.form["confirmpassword"]
+      hash_password = generate_password_hash(password,method="sha256")
+      print(hash_password)
       cur = mysql.connection.cursor()
       #cur.execute('''CREATE TABLE users (username VARCHAR(30),email VARCHAR(50),password VARCHAR(100))''')
-      cur.execute("INSERT INTO  users (username,email,password) VALUES(%s,%s,%s)",(username,email,password))
+      cur.execute("INSERT INTO  users (username,email,password) VALUES(%s,%s,%s)",(username,email,hash_password))
       mysql.connection.commit()
       # session["username"] = username      
       cur.close()
@@ -62,7 +73,17 @@ def register():
 
 @app.route('/forgotpassword',methods=["GET","POST"])
 def forgotpassword():
-   return "forgotpassword"
+   if request.method == "POST":
+      email = request.form["email"]
+      cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+      # cur.execute("SELECT * FROM users WHRERE username=%s",[username])
+      cur.execute( "SELECT * FROM users WHERE email LIKE %s", (email,) )
+      user = cur.fetchone()
+      print(user)
+      cur.close()
+      
+      return "please check mail"
+   return render_template("forgotpassword.html")
 
 @app.route('/database')
 def database():
@@ -75,6 +96,10 @@ def database():
       return "Table is created"
    except OperationalError:
       return "Table already existed"
+
+
+
+
 
 if __name__ == '__main__':
    app.run(debug=True)
